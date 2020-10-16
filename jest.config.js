7bc5b5fb0b9a31116568a7479342b833007c364e
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2020-10-16 11:40:24 +0200

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -6,8 +6,5 @@ module.exports = {
   collectCoverageFrom: ['**/*.ts?(x)', '!**/*.d.ts', '!jest/**/*'],
   setupFilesAfterEnv: ['./jest/setupTests.ts'],
   snapshotResolver: './jest/snapshotResolver.js',
-  snapshotSerializers: ['enzyme-to-json/serializer'],
-  transform: {
-    '^.+\\.[t|j]sx?$': 'babel-jest'
-  }
+  snapshotSerializers: ['enzyme-to-json/serializer']
 };
