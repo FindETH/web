7e0fc85b49af1661df0c43381834189a8932629a
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2020-10-17 18:42:23 +0200

    Fix tests

--- src/features/flow/Flow.test.tsx
+++ src/features/flow/Flow.test.tsx
@@ -1,4 +1,3 @@
-import { mount, shallow } from 'enzyme';
 import createMockStore from 'redux-mock-store';
 import { setFlow } from '../../store/derivation';
 import { getComponent } from '../../utils/test-utils';
@@ -9,29 +8,33 @@ const mockStore = createMockStore();
 
 describe('Flow', () => {
   it('passes a onNext and onReset callback function to the components', () => {
-    const component = shallow(<Flow components={[FirstComponent, SecondComponent]} onDone={jest.fn} />);
+    const store = mockStore();
+    const component = getComponent(<Flow components={[FirstComponent, SecondComponent]} onDone={jest.fn} />, store);
+    const first = component.find(FirstComponent);
 
-    expect(component.prop('onNext')).toBeDefined();
-    expect(component.prop('onReset')).toBeDefined();
+    expect(first.prop('onNext')).toBeDefined();
+    expect(first.prop('onReset')).toBeDefined();
   });
 
   it('renders the current step in the flow', () => {
-    const component = shallow(<Flow components={[FirstComponent, SecondComponent]} onDone={jest.fn} />);
+    const store = mockStore();
+    const component = getComponent(<Flow components={[FirstComponent, SecondComponent]} onDone={jest.fn} />, store);
 
     expect(component.find(FirstComponent)).toBeDefined();
   });
 
   it('renders the next component when onNext is called', () => {
-    const component = mount(<Flow components={[FirstComponent, SecondComponent]} onDone={jest.fn} />);
+    const store = mockStore();
+    const component = getComponent(<Flow components={[FirstComponent, SecondComponent]} onDone={jest.fn} />, store);
     component.find('#next-button').simulate('click');
 
     expect(component.find(SecondComponent)).toBeDefined();
   });
 
   it('calls onDone when all steps are done', () => {
+    const store = mockStore();
     const fn = jest.fn();
-
-    const component = mount(<Flow components={[FirstComponent, SecondComponent]} onDone={fn} />);
+    const component = getComponent(<Flow components={[FirstComponent, SecondComponent]} onDone={fn} />, store);
     component.find('#next-button').simulate('click');
     component.find('#next-button').simulate('click');
 
@@ -39,7 +42,8 @@ describe('Flow', () => {
   });
 
   it('goes back to the first step when onReset is called', () => {
-    const component = mount(<Flow components={[FirstComponent, SecondComponent]} onDone={jest.fn()} />);
+    const store = mockStore();
+    const component = getComponent(<Flow components={[FirstComponent, SecondComponent]} onDone={jest.fn} />, store);
     component.find('#next-button').simulate('click');
     component.find('#reset-button').simulate('click');
 
@@ -48,7 +52,7 @@ describe('Flow', () => {
 
   it('updates the flow state in the store', () => {
     const store = mockStore();
-    const component = getComponent(<Flow components={[FirstComponent]} onDone={jest.fn()} />, store);
+    const component = getComponent(<Flow components={[FirstComponent]} onDone={jest.fn} />, store);
     component.find('#next-button').simulate('click');
 
     expect(store.getActions()).toContainEqual(setFlow(true));
