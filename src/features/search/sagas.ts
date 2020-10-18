3cc440b4596c6112e45c79bbd17c18882b403c3c
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2020-10-18 23:56:45 +0200

    Move search logic to a web worker

--- src/features/search/sagas.ts
+++ src/features/search/sagas.ts
@@ -1,33 +1,33 @@
-import { DerivationPath, deserialize, getFullPath, Wallet } from '@findeth/wallets';
+import { DerivationPath } from '@findeth/wallets';
 import { SagaIterator } from 'redux-saga';
 import { all, call, fork, put, select, takeEvery } from 'redux-saga/effects';
 import { ApplicationState } from '../../store';
 import { SerialisedWallet } from '../../types/wallet';
+import SearchWorker from './search.worker.ts';
 import { addDerivedAddress, startSearching } from './types';
 
 interface GetAddressesAction {
-  wallet: Wallet;
+  wallet: SerialisedWallet;
   derivationPaths: DerivationPath[];
   depth: number;
 }
 
 export function* getAddresses({ wallet, derivationPaths, depth }: GetAddressesAction): SagaIterator {
+  // TODO: Pass serialised wallet to function
+  const worker = new SearchWorker();
+
   for (const derivationPath of derivationPaths) {
     for (let index = 0; index < depth; index++) {
-      const address = yield call([wallet, wallet.getAddress], derivationPath, index);
-      const result = { address, derivationPath: getFullPath(derivationPath, index) };
-
+      const result = yield call([worker, worker.deriveAddress], wallet, derivationPath, index);
       yield put(addDerivedAddress(result));
     }
   }
 }
 
 export function* searchSaga(): SagaIterator {
-  const implementation: SerialisedWallet = yield select((state: ApplicationState) => state.search.wallet);
+  const wallet: SerialisedWallet = yield select((state: ApplicationState) => state.search.wallet);
   const derivationPaths: DerivationPath[] = yield select((state: ApplicationState) => state.search.derivationPaths);
   const depth: number = yield select((state: ApplicationState) => state.search.depth);
-
-  const wallet: Wallet = deserialize(implementation);
 
   // TODO: Task cancellation
   /*const task: Task = */ yield fork(getAddresses, { wallet, derivationPaths, depth });
