a106b71e1a833d5792e8f48e3b106a1d61eff219
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-01-22 18:19:09 +0100

    Replace fontsource-* with @fontsource/*

--- gatsby/gatsby-browser.ts
+++ gatsby/gatsby-browser.ts
@@ -1,3 +1,3 @@
-import 'fontsource-inter/latin-400-normal.css';
-import 'fontsource-inter/latin-500-normal.css';
-import 'fontsource-inter/latin-700-normal.css';
+import '@fontsource/inter/latin-400.css';
+import '@fontsource/inter/latin-500.css';
+import '@fontsource/inter/latin-700.css';
