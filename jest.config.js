91005807c26ceec49429c6d0ad49bbb5d8b6b412
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-07-14 14:39:07 +0200

    Exclude E2E tests in coverage

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   moduleNameMapper: {
     '\\.(woff|woff2|svg)$': '<rootDir>/jest/fileMock.ts'
   },
-  collectCoverageFrom: ['**/*.ts?(x)', '!**/*.d.ts', '!jest/**/*'],
+  collectCoverageFrom: ['**/*.ts?(x)', '!**/*.d.ts', '!**/*.test.e2e.ts?(x)', '!jest/**/*'],
   setupFiles: ['jest-canvas-mock'],
   setupFilesAfterEnv: ['./jest/setupTests.ts'],
   snapshotResolver: './jest/snapshotResolver.js',
